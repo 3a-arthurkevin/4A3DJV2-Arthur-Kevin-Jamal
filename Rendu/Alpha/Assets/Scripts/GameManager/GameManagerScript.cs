753e7ebcd9779c429fa7296f3abaebabb3b52c2f
3a-arthurkevin <torrente.arthur@gmail.com>
2014-12-10 23:54:42 +0100

    Ajout GameManager dans chaque scene ajout event click pour passer les splashscreen

---
+++ Rendu/Alpha/Assets/Scripts/GameManager/GameManagerScript.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * @Author : Arthur TORRENTE
+ * @Date : 10/12/2014
+ * @Desc : GameManager, utiliser pour la persistance des données lors des chargement de scènes
+ * @LastModifier : Arthur TORRENTE
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour
+{
+    /**
+     * Instance de persistance des données durant les chargements de scène
+     */
+    public static GameManagerScript m_gameManager;
+
+    /**
+     * Check Global instance is same instance of this
+     */
+    void Awake()
+    {
+        if(m_gameManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            m_gameManager = this;
+        }
+        else if(m_gameManager != this)
+        {
+            MergeGameManagerInstance();
+            Destroy(gameObject);
+        }
+    }
+
+    /**
+     * Remplace les GameObject de la scène précédente par les versions de scène nouvellement chargé
+     * (Ex : Camera de scene, Player, ...)
+     */
+    void MergeGameManagerInstance()
+    {
+
+    }
+	
+}
