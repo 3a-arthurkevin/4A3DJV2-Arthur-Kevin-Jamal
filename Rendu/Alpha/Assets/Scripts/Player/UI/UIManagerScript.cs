7eabdec0a50093bcd8ef115ed0993082daeccf98
3a-arthurkevin <torrente.arthur@gmail.com>
2014-12-13 16:45:17 +0100

    Ajout des headers dans chaque script

--- Rendu/Alpha/Assets/Scripts/Player/UI/UIManagerScript.cs
+++ Rendu/Alpha/Assets/Scripts/Player/UI/UIManagerScript.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿/**
+ * @Author : Arthur TORRENTE
+ * @Date : 07/12/2014
+ * @Desc : 
+ * @LastModifier : Arthur TORRENTE
+ */
+
+
+using UnityEngine;
 using System.Collections;
 
 public class UIManagerScript : MonoBehaviour
--- Rendu/Alpha/Assets/Scripts/Utils/Constants.cs
+++ Rendu/Alpha/Assets/Scripts/Utils/Constants.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/**
+ * @Author : Arthur TORRENTE
+ * @Date : 07/12/2014
+ * @Desc : Classe de centralisation des constantes
+ * @LastModifier : Arthur TORRENTE
+ */
+
+using UnityEngine;
 using System.Collections;
 
 public static class Constants
