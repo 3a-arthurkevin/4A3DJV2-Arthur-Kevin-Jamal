eb091c3305d5cf280b764167367d7d843bd69d45
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-12-14 22:55:06 +0100

    Commit anim player
    >Creation d'une scene pour les animations personnage
    >Création des animations et animator et state machin
    >Modification du prefab Player (ajout de 2 spheres pour représenter
    poing et pied)
    >Ajout d'un rigidbody syr le player
    >Collider player Triggerable

---
+++ Rendu/Alpha/Assets/Scripts/Player/Animation/CharacterAnimationsScript.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * @Author : Kevin WATHTHUHEWA
+ * @Date : 13/12/2014
+ * @Desc : Script Lancant une animation du joueur selon son action
+ * @LastModifier : Kevin WATHTHUHEWA
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimationsScript : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_playerAnimator;
+
+    /*
+     *      \\ Player Animation ActionId Value //
+     * 1    WalkForward
+     * 2    WalkBackward
+     * 3    Guard
+     * 4    Jump
+     * 5    HighPunch
+     * 6    LowPunch
+     * 7    HighKick
+     * 8    LowKick
+     * 9    TakeDamage
+     */
+
+    public void lunchAction(int value)
+    {
+        m_playerAnimator.SetInteger("IdAction", value);
+    }
+}
