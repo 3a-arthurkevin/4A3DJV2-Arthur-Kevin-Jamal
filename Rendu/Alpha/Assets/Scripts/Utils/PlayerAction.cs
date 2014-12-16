f09ba3f6894ac60941d67890514fedc744f0b22b
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-12-16 02:23:31 +0100

    Commit fin update animation

--- Rendu/Alpha/Assets/Scripts/Utils/PlayerAction.cs
+++ Rendu/Alpha/Assets/Scripts/Utils/PlayerAction.cs
@@ -10,13 +10,14 @@ using System.Collections;
 
 public enum PlayerAction
 {
-    None,
-    MoveForward,
-    MoveBackWard,
-    Jump,
-    HighPunch,
-    LowPunch,
-    HighKick,
-    LowKick,
-    Guard
+    None,           //0
+    MoveForward,    //1
+    MoveBackWard,   //2
+    Jump,           //3
+    Guard,          //4
+    HighPunch,      //5
+    LowPunch,       //6
+    HighKick,       //7
+    LowKick,        //8
+    TakeDamage      //9
 }
